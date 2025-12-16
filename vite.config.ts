a930a80f0c4ed9d5728be2d26558f95ee2d8b490
Luciano Freitas <luciano.freitas@nomadic-labs.com>
2025-12-16 08:26:15 +0100

    Fix polyfill issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills({
+      protocolImports: true, // supports "node:crypto" etc
+    }),
+  ],
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+      stream: "stream-browserify",
+      http: "stream-http",
+      https: "https-browserify",
+      util: "util",
+      zlib: "browserify-zlib",
+    },
+  },
+  define: {
+    global: "globalThis",
+  },
+});
